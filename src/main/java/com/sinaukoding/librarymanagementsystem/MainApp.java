b268fc5b001af4ff35d7685cf0a8839caa15de6f
train-do <trainnando.mail@gmail.com>
2025-09-12 19:14:52 +0700

    feat: adding builder, config, util (plug and play)

--- src/main/java/com/sinaukoding/librarymanagementsystem/MainApp.java
+++ src/main/java/com/sinaukoding/librarymanagementsystem/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementSystemApplication {
+public class MainApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryManagementSystemApplication.class, args);
+        SpringApplication.run(MainApp.class, args);
     }
 
 }
